b1e755d4afb408df66034232f5f99999610d60de
thetime50 <1004763777@qq.com>
2019-04-12 03:34:26 +0800

    index.styl 定义$blbl-search-pink MainPage.vue 完成headerTop布局

--- bilibililike-client/src/pages/MainPage/MainPage.vue
+++ bilibililike-client/src/pages/MainPage/MainPage.vue
@@ -2,16 +2,27 @@
 <div class="main-page">
   <header-top lift-icon-type="liftMenu">
     <template v-slot:liftContent>
-      l
-    </template>
-    <template v-slot:centerContent>
-      <p>ccccc</p>
+      <div class="heater-v-slot">
+        <span class="m-search"><!-- search -->
+          <div class="search-back-div">
+            <i class="iconfont icon-sousuo"></i>
+          </div>
+        </span>
+      </div>
     </template>
     <template v-slot:rightContent>
-      rr
+      <div class="heater-v-slot">
+        <span class="m-game"><!-- game -->
+          <i class="iconfont icon-youxifull"></i>
+        </span>
+        <span class="m-offline-cache"><!-- offline-cache -->
+          <i class="iconfont icon-xiazai"></i>
+        </span>
+        <span class="m-message"><!-- message -->
+          <i class="iconfont icon-xiaoxo"></i>
+        </span>
+      </div>
     </template>
-    <!-- v-slot:default -->
-    d
   </header-top>
   MainPage
 </div>
@@ -33,4 +44,35 @@ export default {
 </script>
 
 <style lang="stylus"  rel="stylesheet/stylus">
+  @import "../../assets/style/index.styl"
+  .main-page .header .div-section1
+    width 100%
+    .liftContent
+      width 70%
+      .heater-v-slot
+        &>[m-*]
+          float left
+          display: table-cell;
+          vertical-align: middle;//没啥效果??
+        .m-search //todo 这里回头加到henderTop组件里
+          float left
+          width 100%
+          .search-back-div
+            position relative
+            top 0.2rem
+            margin-left 0.4rem
+            width 100%
+            height $com-iconsize*1.6
+            background-color $blbl-search-pink
+            border-radius $com-iconsize
+            text-align left
+            padding-left $com-iconsize*0.4
+            .iconfont
+              margin 0
+              position relative
+              top -0.25rem
+              opaticy 0.1
+    .m-game .iconfont.icon-youxifull
+      font-size $com-iconsize*1.2
+
 </style>
