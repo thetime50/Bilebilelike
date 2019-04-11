c0637d93bc94f08c2198ab119d35d397e84b6946
thetime50 <1004763777@qq.com>
2019-04-12 04:22:16 +0800

    改用flex布局

--- bilibililike-client/src/pages/MainPage/MainPage.vue
+++ bilibililike-client/src/pages/MainPage/MainPage.vue
@@ -1,17 +1,13 @@
 <template>
 <div class="main-page">
   <header-top lift-icon-type="liftMenu">
-    <template v-slot:liftContent>
+    <template>
       <div class="heater-v-slot">
         <span class="m-search"><!-- search -->
           <div class="search-back-div">
             <i class="iconfont icon-sousuo"></i>
           </div>
         </span>
-      </div>
-    </template>
-    <template v-slot:rightContent>
-      <div class="heater-v-slot">
         <span class="m-game"><!-- game -->
           <i class="iconfont icon-youxifull"></i>
         </span>
@@ -19,7 +15,7 @@
           <i class="iconfont icon-xiazai"></i>
         </span>
         <span class="m-message"><!-- message -->
-          <i class="iconfont icon-xiaoxo"></i>
+          <i class="iconfont icon-xiaoxi"></i>
         </span>
       </div>
     </template>
@@ -45,33 +41,38 @@ export default {
 
 <style lang="stylus"  rel="stylesheet/stylus">
   @import "../../assets/style/index.styl"
-  .main-page .header .div-section1//尽量外面套自己的class 不要去引内部的class
+  .main-page .header .div-section2//尽量外面套自己的class 不要去引内部的class
     width 100%
-    .liftContent
-      width 70%
-      .heater-v-slot
-        &>[m-*]
-          float left
-          display: table-cell;
-          vertical-align: middle;//没啥效果??
-        .m-search //todo 这里回头加到henderTop组件里
-          float left
-          width 100%
-          .search-back-div
-            width 100%
-            height $com-iconsize*1.6
+    .heater-v-slot
+      display: -webkit-flex; /* Safari */
+      display: flex;
+      width 100%
+      // &>[m-*]
+      //   display: table-cell;
+      //   vertical-align: middle;//没啥效果??
+      .m-search //todo 这里回头加到henderTop组件里
+        flex 6
+        .search-back-div
+          width auto
+          height $com-iconsize*1.6
+          position relative
+          top @height*0.1
+          margin-left 0.4rem
+          background-color $blbl-search-pink
+          border-radius @height*0.5
+          text-align left
+          padding-left @height*0.27
+          .iconfont
+            margin 0
             position relative
-            top @height*0.1
-            margin-left 0.4rem
-            background-color $blbl-search-pink
-            border-radius @height*0.5
-            text-align left
-            padding-left @height*0.27
-            .iconfont
-              margin 0
-              position relative
-              top -@height*0.15
-              opaticy 0.1
+            top -@height*0.15
+            opaticy 0.1
+      .m-game
+        flex 1
+      .m-offline
+        flex 1
+      .m-message
+        flex 1
     .m-game .iconfont.icon-youxifull
       font-size $com-iconsize*1.2
 
