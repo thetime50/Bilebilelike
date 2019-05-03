bcc9786ea16fe771108b39e9d8c75cf5328f2ca5
thetime50 <1004763777@qq.com>
2019-05-03 20:48:27 +0800

    又是初步的左侧菜单

--- bilibililike-client/src/components/LeftMenu/LeftMenu.vue
+++ bilibililike-client/src/components/LeftMenu/LeftMenu.vue
@@ -2,9 +2,28 @@
 <div class="component-left-menu">
   <!-- scroll -->
   <div class="left-menu-scroll" ref="left-menu-scroll">
-    <div class="scroll-container">
-      <div v-for="(item,index) in mainMenu" :key="index">
-        {{item}}
+    <div class="left-menu">
+      <div class="left-menu-header">
+        header
+      </div>
+      <div class="left-menu-bigvip">
+        大会员
+      </div>
+      <div class="left-menu-wrapper">
+        wrapper
+      </div>
+      <div class="left-menu-groups">
+        <div class="left-menu-group" v-for="(group,gindex) in mainMenuGroup" :key="gindex">
+          <hr v-if="gindex">
+          <div class="left-menu-item" v-for="(item,index) in group" :key="index">
+            <div class="item-container" @click="pushItem(item)">
+              <div class="item-icon">
+                <i class="iconfont" :class="item.icon"></i>
+              </div>
+              <div class="item-text">{{item.text}}</div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -34,11 +53,28 @@ export default {
   data () {
     return {
       //data
-      mainMenu:"123fsagwgxcfd".repeat(3),
+      mainMenuGroup:[
+        [
+          {text:"首页"      ,icon:"icon-zhuye",path:""},
+          {text:"历史记录"  ,icon:"icon-lishi",path:""},
+          {text:"离线缓存"  ,icon:"icon-lixianhuancun",path:""},
+          {text:"我的收藏"  ,icon:"icon-shoucang",path:""},
+          {text:"稍后再看"  ,icon:"icon-shaohouzaikan",path:""},
+        ],[
+          {text:"投稿"      ,icon:"icon-tougao",path:""},
+          {text:"创作中心"  ,icon:"icon-chuangzuozhongxin",path:""},
+          {text:"热门活动"  ,icon:"icon-huodong",path:""},
+        ],[
+          {text:"直播中心"    ,icon:"icon-zhibo",path:""},
+          {text:"免流量服务"  ,icon:"icon-mianliuliangfuwu",path:""},
+          {text:"我的订单"    ,icon:"icon-wodedindan",path:""},
+          {text:"会员购中心"  ,icon:"icon-huiyuangouzhongxin",path:""},
+          {text:"联系客服"    ,icon:"icon-lianxikefu",path:""},
+        ]],
       footerItem:[
-        {path:"" ,icon:"icon-shezhi",text:"设置"},
-        {path:"" ,icon:"icon-zhuti",text:"主题"},
-        {path:"" ,icon:"icon-yejianmoshi",text:"夜间"},
+        {text:"设置",icon:"icon-shezhi"     ,path:"",},
+        {text:"主题",icon:"icon-zhuti"      ,path:"",},
+        {text:"夜间",icon:"icon-yejianmoshi",path:"",},
       ],
 
       //state
@@ -54,15 +90,23 @@ export default {
     }
   },
   mounted () {
-    this.$nextTick(() =>{
+    this.$nextTick(function() {
       this.scroll = new BScroll(this.$refs["left-menu-scroll"],{ 
         click: true,
         bounce:false,
         bindToWrapper:true,
         // stopPropagation:true,
       })
+      //不知道为什么这里一定要重新刷新
+      setTimeout(()=>this.scroll.refresh(),120)
     })
   },
+  methods: {
+    pushItem (item) {
+      let path=item.path?item.path:{path:'/defpage',query:{text:item.text}}
+      this.$router.push(path)
+    },
+  }
 }
 </script>
 
@@ -70,36 +114,66 @@ export default {
 @import "../../assets/style/index.styl"
 
 .component-left-menu
-  *
-    border 0.2px solid #088
+  // *
+  //   border 0.2px solid #088
+  display grid
+  grid-template-columns 1fr
+  grid-template-rows 1fr 4rem
+  place-items stretch stretch
+  height 100%
   .left-menu-scroll
-    height 20rem
     overflow hidden
+    .left-menu
+      .left-menu-header
+        height 9rem
+      .left-menu-bigvip
+        height 3rem
+      .left-menu-wrapper
+        height 5rem
+      .left-menu-groups
+        .left-menu-group
+          .left-menu-item
+            display table
+            height 3.5rem
+            width 100%
+            .item-container
+              position relative
+              display table-cell
+              vertical-align middle
+              text-align left
+              .item-icon
+                display inline-block
+                position absolute
+                left 1.5rem
+                .iconfont
+                  font-size 1.4rem
+              .item-text
+                display inline-block
+                position absolute
+                left 4rem
+                font-size 1.1rem
   .menu-footer
-    position absolute
-    bottom 0
-    height 4rem
-    width 100%
     display grid
     grid-template-columns repeat(3,1fr)
     grid-template-rows 1rm
     place-items center center
-  .menu-footer-item
-    color $def-color
-    .item-container
-      .item-icon
-        display inline-block
-        vertical-align: middle
-        width 2rem
-        height 2rem
-        border 1px solid $def-color
-        border-radius 50%
-        i
+    background-color #fff
+    .menu-footer-item
+      color $def-color
+      .item-container
+        .item-icon
+          display inline-block
+          vertical-align: middle
+          width 2rem
+          height 2rem
+          border 1px solid $def-color
+          border-radius 50%
+          i
+            display: inline-block
+            vertical-align: middle
+            line-height 2rem
+            font-size 1.5rem
+        .item-text
           display: inline-block
           vertical-align: middle
-          line-height 2rem
-          font-size 1.5rem
-      .item-text
-        display: inline-block
-        vertical-align: middle
 </style>
