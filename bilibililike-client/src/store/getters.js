c81d85b1790aeb80eca27e70e4504930275eb2a9
thetime50 <1004763777@qq.com>
2019-05-13 08:13:18 +0800

    gitpage上static图片只能相对路径 build

--- bilibililike-client/src/store/getters.js
+++ bilibililike-client/src/store/getters.js
@@ -1,7 +1,7 @@
 //基础属性
 export default{
   headImgGetter:function(state,getters){
-    return (state.userInfo?state.userInfo.headImg:
+    return "../../.."+(state.userInfo?state.userInfo.headImg:
       "/static/temp/imgs/bili_default_avatar.png")},
   bigvipGetter:function(state,getters){
     return state.userInfo?state.userInfo.bigvip:false}
