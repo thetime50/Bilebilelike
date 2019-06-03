81abf3f96456e492810731454ae71797b30ae6e1
thetime50 <1004763777@qq.com>
2019-06-03 14:43:43 +0800

    完成StickyScroll.vue

--- bilibililike-client/src/App.vue
+++ bilibililike-client/src/App.vue
@@ -4,15 +4,21 @@
     <div><i class="iconfont icon-pindao" style="font-size:100px;color:#fb7299"></i></div> -->
     <DbgMsg/>
     <left-menu-drawer></left-menu-drawer>
-    <div class="app-router-view-contain" ref="app-router-view-contain">
-        <router-view/>
-    </div>
-    <div class="app-nav-list" ref="app-nav-list">
-      <NavList v-if="nav_enable"
-        :nowIndex="nav_nowIndex"
-        :cursorWidth="nav_cursorWidth"
-        :positionSync="nav_positionSync"
-        :routes="nav_routes"/>
+    <div class="app-sticky-scroll-contain">
+      <StickyScroll>
+        <template>
+          <router-view/>
+        </template>
+        <template v-slot:sticky>
+          <div class="app-nav-list" ref="app-nav-list">
+            <NavList v-if="nav_enable"
+              :nowIndex="nav_nowIndex"
+              :cursorWidth="nav_cursorWidth"
+              :positionSync="nav_positionSync"
+              :routes="nav_routes"/>
+          </div>
+        </template>
+      </StickyScroll>
     </div>
     <FooterGuide v-show="$route.meta.showFooter"/>
   </div>
@@ -22,11 +28,11 @@
 import {mapState} from "vuex"
 import tool from "./assets/js/tool"
 
-import BScroll from "better-scroll"
 import LeftMenuDrawer from "./components/LeftMenuDrawer/LeftMenuDrawer.vue"
 import FooterGuide from "./components/FooterGuide/FooterGuide.vue"
 import DbgMsg from "./components/DbgMsg/DbgMsg.vue"
 import NavList from "./components/RouteScroll/Nav.vue"
+import StickyScroll from "./components/StickyScroll/StickyScroll.vue"
 
 export default {
   name: 'App',
@@ -35,40 +41,9 @@ export default {
     FooterGuide,
     DbgMsg,
     NavList,
+    StickyScroll,
   },
   mounted () {
-    // this.$nextTick(()=> {
-    //   console.dir(this.$refs["app-router-view-contain"].innerHTML.slice(0,40))
-    //   this.scroll = new BScroll(this.$refs["router-view-contain"],{ 
-    //     click: true,
-    //     bounce:false,
-    //     bindToWrapper:true,
-    //     // stopPropagation:true,
-    //   })
-    //   //不知道为什么这里一定要重新刷新
-    //   setTimeout(()=>this.scroll.refresh(),120)
-    // })
-    this._interval=setInterval(()=>{
-      // console.dir(this.$refs["app-router-view-contain"].innerHTML.length)
-      if(this.$refs["app-router-view-contain"].innerHTML.length>100){
-        clearInterval(this._interval)
-        this.scroll = new BScroll(this.$refs["app-router-view-contain"],{ 
-          click: true,
-          bounce:false,
-          bindToWrapper:true,
-          probeType:3,
-          // stopPropagation:true,
-        })
-        this.scroll.on('scroll',({x,y})=>{
-          let appNavList=this.$refs["app-nav-list"]
-          let childrenTop=appNavList.children[0].offsetTop
-          if(childrenTop+y>0)
-            appNavList.style.top=y+"px"
-          else
-            appNavList.style.top=-childrenTop+"px"
-        })
-      }
-    },10)
   },
   computed:{
     ...mapState("navState",[
@@ -117,17 +92,13 @@ img
 #app
   position relative
   height 100%
-  .app-router-view-contain
+  .app-sticky-scroll-contain
     overflow hidden
     top 0
     bottom $footer-guide-height
     width 100%
     position absolute
-#app
-  &>.app-nav-list
-    position fixed
-    width 100%
-    &>.component-nav
+    .app-nav-list
       position absolute
       top $header-top-height
       width 100%
---
+++ bilibililike-client/src/components/StickyScroll/StickyScroll.vue
@@ -0,0 +1,79 @@
+<!--
+StickyScroll.vue
+-->
+<template>
+<div class="component-sticky-scroll">
+  <div class="sticky-scroll-scroll" ref="sticky-scroll-scroll">
+    <slot></slot>
+  </div>
+  <div class="sticky-scroll-sticky" ref="sticky-scroll-sticky">
+    <slot name="sticky"></slot> <!-- position absolute must -->
+  </div>
+</div>
+</template>
+
+<script>
+import BScroll from "better-scroll"
+
+export default {
+  name: "StickyScroll",
+  data () {
+    return {
+    };
+  },
+  mounted () {
+    // this.$nextTick(()=> {
+    //   console.dir(this.$refs["app-router-view-contain"].innerHTML.slice(0,40))
+    //   this.scroll = new BScroll(this.$refs["router-view-contain"],{ 
+    //     click: true,
+    //     bounce:false,
+    //     bindToWrapper:true,
+    //     // stopPropagation:true,
+    //   })
+    //   //不知道为什么这里一定要重新刷新
+    //   setTimeout(()=>this.scroll.refresh(),120)
+    // })
+    this._interval=setInterval(()=>{
+      // console.dir(this.$refs["sticky-scroll-scroll"].innerHTML.length)
+      if(this.$refs["sticky-scroll-scroll"].innerHTML.length>100){
+        clearInterval(this._interval)
+        this.scroll = new BScroll(this.$refs["sticky-scroll-scroll"],{ 
+          click: true,
+          bounce:false,
+          bindToWrapper:true,
+          probeType:3,
+          // stopPropagation:true,
+        })
+        this.scroll.on('scroll',({x,y})=>{
+          this.stickyTopRefresh(y)
+        })
+        this.stickyTopRefresh(0)
+      }
+    },10)
+  },
+  methods: {
+    stickyTopRefresh(y){
+      let appNavList=this.$refs["sticky-scroll-sticky"]
+      let childrenTop=appNavList.children[0].offsetTop
+      if(childrenTop+y>0)
+        appNavList.style.top=y+"px"
+      else
+        appNavList.style.top=-childrenTop+"px"
+    }
+  }
+}
+</script>
+
+<style lang="stylus"  rel="stylesheet/stylus">
+.component-sticky-scroll
+  position relative
+  width 100%
+  height 100%
+  .sticky-scroll-scroll
+    position relative
+    width 100%
+    height 100%
+  .sticky-scroll-sticky
+    position absolute
+    width 100%
+</style>
