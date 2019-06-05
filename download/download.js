4dc51ffdf7ca1d2c68f501a524301742623dd569
thetime50 <1004763777@qq.com>
2019-06-05 17:43:08 +0800

    下载脚本添加recommendnew和reply

--- download/download.js
+++ download/download.js
@@ -4,13 +4,11 @@ var fs = require('fs');
 const util = require('util');
 var reqPath = require('path')
 
-var json_path = '../docs/reference/pages/main/data/ranking'
 var dowmload_path = "download"// '/'开头是当前盘符 不写是当前目录
 // process.cwd() 当前执行程序的路径（执行命令行时候的路径,不是代码路径 例如 在根目录下执行 node ./xxx/xxx/a.js 则 cwd 返回的是 根目录地址 ）
 // __dirname: 代码存放的位置
 // process.execPath: 当前执行的node路径（如：/bin/node）
 
-var webp = "@320w_200h.webp"
 var fileStr=function(f){
   var obj=f.lastIndexOf("/")
   obj=obj<0?lastIndexOf("\\"):obj
@@ -55,18 +53,82 @@ var bagpipe_down = function (imgList,max) {
   }
 }
 
-var main=function(){
-  let json=readJsonFile(json_path)
-  let imgList=json.data.list.map((val,index,arr)=>{
-    //"http://i0.hdslb.com/bfs/archive/7917b40671d87ec6d66a8b5d9bceb5de40aa0ee5.jpg"
-    let file=val.pic.replace(/^https?:\/\//,"")
-    let hdslbRe=/^i\d\.hdslb\.com\b/
-    file=file.replace(hdslbRe,"i0.hdslb.com")
-    // file=reqPath.join(__dirname,file)
-    file=dowmload_path+"/"+file
-    return {img:val.pic+webp,file:file}
-  })
+/////////////////////////////////////////////////
+const getAttribute = function (obj, attrPath,type=0) {
+  let attr=obj
+  let paths=[]
+  if(typeof attrPath === "string")
+    paths=attrPath.split(/\.|\[|\]/)
+  else if(typeof attrPath === "array")
+    paths=attrPath
+  for(let i=0;i<paths.length;i++){
+    if(paths[i]=="")
+      continue
+    if(attr[paths[i]]!=undefined){//存在属性
+      attr=attr[paths[i]]
+    }else{
+      if(type==0){
+        attr=attr[paths[i]]//undefined
+        break
+      }else{
+        attr[paths[i]]=i<paths.length-1?{}:undefined
+        attr=attr[paths[i]]
+      }
+    }
+  }
+  return attr
+}
+
+function makeImgList (f_path,list_path,attr_paths,imgSuffix=""){
+  let hdslbRe=/^i\d\.hdslb\.com\b/
+  let json=readJsonFile(f_path)
+  let imgList=[]
+  let list=getAttribute(json,list_path)
+  for(let i=0;i<list.length;i++){
+    let val=list[i]
+    attr_paths.forEach((apath,index,arr) => {
+      let attr=getAttribute(val,apath)
+      if(typeof(attr)!="string" || !attr)
+        return
+      let file=attr.replace(/^(https?:)?\/\//,"")
+      file=file.replace(hdslbRe,"i0.hdslb.com")
+      file=dowmload_path+"/"+file
+
+      let img=attr+imgSuffix
+      if(/^\/\//.test(attr))
+        img="http:"+img
+      imgList.push({img,file})
+    });
+  }
+  return imgList
+}
+
+var ranking_path = '../docs/reference/pages/main/data/ranking'
+var video_webp = "@320w_200h.webp"
+var ranking_down=function(){
+  let imgList=makeImgList (ranking_path,"data.list",["pic"],video_webp)
   bagpipe_down(imgList,60)
 }
 
-main()
+// var av_serial = "51811079"
+var av_serial = "53498266"
+var av_path ='../docs/reference/pages/av'+av_serial+'/data'
+var recommendnew_path = av_path + '/recommendnew,'+av_serial+'.json'
+var recommendnew_down=function(){
+  let imgList=makeImgList (recommendnew_path,"data",["pic"/*,"owner.face"*/])
+  bagpipe_down(imgList,20)
+}
+
+var reply_path = av_path + '/reply.json'
+var reply_down=function(){
+  let imgList=makeImgList (reply_path,"data.replies",[
+    "member.avatar",
+    //  "member.pendant.image",
+    // "member.nameplate.image_small",
+  ])
+  bagpipe_down(imgList,20)
+}
+
+// ranking_down()
+recommendnew_down()
+reply_down()
