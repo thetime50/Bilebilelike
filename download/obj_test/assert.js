4ac2f8855b0504a92648774fbe226a711230545e
thetime50 <1004763777@qq.com>
2019-06-09 11:07:18 +0800

    iterationTest_1 iterationTest_2

--- download/obj_test/assert.js
+++ download/obj_test/assert.js
@@ -7,6 +7,9 @@ assert.js
 const assert = require("assert")
 const { iteration,objToolCreater } = require("./obj_test.js")
 
+function putResult(){
+  // console.log(...arguments)
+}
 
 var obja = {
   a: 123, b: 456,
@@ -15,6 +18,9 @@ var obja = {
     y: {
       m: "666", n: "777"
     }
+  },
+  d:{
+    p:"ppp",q:"qqq"
   }
 }
 
@@ -22,14 +28,59 @@ function iterationTest_1() {
   let str = ""
   let cb = function (state) {
     if (state.type == "after") {
-      str = str + state.path.join("/") + "\r\n"
+      if (typeof (state.point) != "object" || state.point == {} ||
+        Array.isArray(state.point) && state.point.lenght==0) {
+        str = str + 
+        state.depth + " " +
+        state.path.join("/") + ": " +
+        state.point + "\r\n"
+      }
     }
   }
   iteration({ obj: obja, cb})
   return str
 }
 
-
-// assert.strictEqual(iterationTest_1, 0);
 console.log("iterationTest_1()")
-console.log(iterationTest_1())
+putResult(iterationTest_1())
+assert.strictEqual(iterationTest_1(), 
+  "1 a: 123\r\n" +
+  "1 b: 456\r\n" +
+  "2 c/x: 233\r\n" +
+  "3 c/y/m: 666\r\n" +
+  "3 c/y/n: 777\r\n" +
+  "2 d/p: ppp\r\n" +
+  "2 d/q: qqq\r\n"
+);
+
+
+function iterationTest_2() {
+  let str = ""
+  let cb = function (state) {
+    if (state.type == "after") {
+      if (typeof (state.point) != "object" || state.point == {} ||
+        Array.isArray(state.point) && state.point.lenght==0) {
+        str = str + 
+        state.depth + " " +
+        state.path.join("/") + ": " +
+        state.point + "\r\n"
+
+        if(state.point==233)
+          return {break:true}
+      }
+    }
+  }
+  iteration({ obj: obja, cb})
+  return str
+}
+
+console.log("iterationTest_2()")
+putResult(iterationTest_2())
+assert.strictEqual(iterationTest_2(), 
+  "1 a: 123\r\n" +
+  "1 b: 456\r\n" +
+  "2 c/x: 233\r\n" +
+  "2 d/p: ppp\r\n" +
+  "2 d/q: qqq\r\n"
+);
+
